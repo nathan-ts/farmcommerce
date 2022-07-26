bcdfc942ee077feff3b096deeee94cef32371029
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-07-26 16:38:58 -0400

    add login methods

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 import React, { Component } from "react";
 import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 
@@ -18,6 +20,42 @@ export default class App extends Component {
     };
     this.routerRef = React.createRef();
   }
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  };
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+  
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+  
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
 
   render() {
     return (
