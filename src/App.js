284428b1c6a131f2a483ffd93295c62efb9d5a84
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-08-03 23:15:07 -0400

    bugfix

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ export default class App extends Component {
                 </Link>
                 <label
                   role="button"
-                  class="navbar-burger burger"
+                  className="navbar-burger burger"
                   aria-label="menu"
                   aria-expanded="false"
                   data-target="navbarBasicExample"
